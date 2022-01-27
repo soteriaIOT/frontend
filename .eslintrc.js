4b7b5d847decbcbb04c8ae8c907c7b52ec47d4d7
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2022-01-27 17:22:05 -0500

    Use typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ["prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import logo from "./assets/logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="splash">
-        <img src={logo}></img>
         <button className="authButton" type="button">
           Log In
         </button>
       </div>
-      {/* <div className="buttonWrapper">
-      </div> */}
     </div>
   );
 }
