f78eba0c5e6e827174225238d84c8b84b1f061c7
Kai Huang <edwardthetrueblade2678@gmail.com>
2022-01-29 17:54:25 -0500

    rough initial draft

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,24 +1,61 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import {Button, Stack, Layout} from '@shopify/polaris';
+import { Button, Stack, Layout } from "@shopify/polaris";
 
-import LoginSignup from '../../components/login-sigup/index';
+import LoginSignup from "../../components/login-sigup/index";
 
-import Logo from '../../assets/logo-with-name.svg';
+import Logo from "../../assets/logo-with-name.svg";
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  background-color: #fff;
+  height: 100px;
+`;
+
+const TopBar = styled.div`
+  width: 100%;
+  margin: 20px 0 20px 0;
+  max-width: 720px;
+`;
 
 function Topbar() {
-    const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false);
 
-    const activator = <Button onClick={() => setActive(!active)}>Open</Button>;
-    return (
-        <Stack distribution="equalSpacing">
-            <img src={Logo} alt="logo" style={{height: '60px'}}/>
-            {activator}
-            <LoginSignup active={active} setActive={(value: boolean) => setActive(value)}/>
+  const activator = (
+    <Button primary onClick={() => setActive(!active)}>
+      Sign Up
+    </Button>
+  );
+  return (
+    <Wrapper>
+      <TopBar>
+        <Stack alignment="center">
+          <Stack.Item fill>
+            <img src={Logo} alt="logo" style={{ height: "60px" }} />
+          </Stack.Item>
+          <Stack.Item>
+            <Button plain monochrome>
+              About
+            </Button>
+          </Stack.Item>
+          <Stack.Item>
+            <Button plain monochrome>
+              FAQ
+            </Button>
+          </Stack.Item>
+          <Stack.Item>{activator}</Stack.Item>
         </Stack>
-    );
+      </TopBar>
+      <LoginSignup
+        active={active}
+        setActive={(value: boolean) => setActive(value)}
+      />
+    </Wrapper>
+  );
 }
 
 export default Topbar;
--- src/pages/frontpage/index.tsx
+++ src/pages/frontpage/index.tsx
@@ -1,13 +1,203 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import {Frame, Page, Card, ResourceList, ResourceItem, TextStyle, Avatar, TextField, Filters, Button, ResourceListSelectedItems, Tooltip, Stack} from '@shopify/polaris';
-import Topbar from '../../components/topbar';
+import {
+  Frame,
+  Page,
+  Card,
+  ResourceList,
+  ResourceItem,
+  TextStyle,
+  Avatar,
+  TextField,
+  Filters,
+  Button,
+  ResourceListSelectedItems,
+  Tooltip,
+  Stack,
+  Heading,
+} from "@shopify/polaris";
+import Topbar from "../../components/topbar";
 
+import bannerImage from "../../assets/banner.png";
+import vulnImage from "../../assets/vuln.png";
+import fleetImage from "../../assets/fleet.png";
+import orchImage from "../../assets/orch.png";
+
+const TopBarWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+`;
+
+const Banner = styled.div`
+  background-image: url(${bannerImage});
+  height: 700px;
+  background-size: cover;
+  display: flex;
+  padding-left: 200px;
+`;
+
+const BannerTextWrapper = styled.div`
+  color: #fff;
+  font-weight: 700;
+  max-width: 720px;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Features = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: #3f3d56;
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 64px;
+  padding: 30px 0 30px 0;
+  padding-bottom: 100px;
+`;
+
+type FeatureProps = {
+  flip?: boolean;
+  textHeader: string;
+  textBody: string;
+  image: string;
+};
+
+type FeatureTextProps = {
+  textHeader: string;
+  textBody: string;
+};
+
+const FeatureText = ({ textHeader, textBody }: FeatureTextProps) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <span
+        style={{
+          fontSize: "42px",
+          lineHeight: "42px",
+          color: "#6C63FF",
+          marginBottom: "20px",
+        }}
+      >
+        {textHeader}
+      </span>
+      <span
+        style={{
+          fontWeight: "normal",
+          fontSize: "18px",
+          lineHeight: "18px",
+          color: "#494949",
+        }}
+      >
+        {textBody}
+      </span>
+    </div>
+  );
+};
+
+const FeatureImage = ({ image }: { image: string }) => {
+  return <img style={{ maxWidth: "400px" }} src={image} />;
+};
+
+const Feature = ({ flip, textHeader, textBody, image }: FeatureProps) => {
+  return (
+    <div
+      style={{
+        marginTop: "100px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ maxWidth: "800px", display: "flex" }}>
+        <div style={{ flexGrow: 1, marginRight: "20px" }}>
+          {flip ? (
+            <FeatureImage image={image} />
+          ) : (
+            <FeatureText textHeader={textHeader} textBody={textBody} />
+          )}
+        </div>
+        <div style={{ flexGrow: 1, marginLeft: "20px" }}>
+          {flip ? (
+            <FeatureText textHeader={textHeader} textBody={textBody} />
+          ) : (
+            <FeatureImage image={image} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function Frontpage() {
-    return (<Topbar />)
-  }
+  return (
+    <div>
+      <TopBarWrapper>
+        <Topbar />
+      </TopBarWrapper>
+      <Banner>
+        <BannerTextWrapper>
+          <div
+            style={{
+              fontSize: "70px",
+              lineHeight: "70px",
+              paddingBottom: "50px",
+            }}
+          >
+            See. Know. Secure.
+          </div>
+          <div
+            style={{
+              fontSize: "36px",
+              lineHeight: "36px",
+              paddingBottom: "20px",
+            }}
+          >
+            Monitor every asset and detect vulnerability and threats.
+          </div>
+          <div
+            style={{
+              fontSize: "36px",
+              lineHeight: "36px",
+              paddingBottom: "20px",
+            }}
+          >
+            Seamlessly.
+          </div>
+        </BannerTextWrapper>
+      </Banner>
+      <Features>
+        Our Features
+        <Feature
+          textBody="Protect your devices by scanning for vulnerabilities regularly and automatically patch them."
+          textHeader="Vulnerability Scanner"
+          image={vulnImage}
+        ></Feature>
+        <Feature
+          flip
+          textBody="Monitor device utilization and be notified when something is off."
+          textHeader="Fleet Reporting"
+          image={fleetImage}
+        ></Feature>
+        <Feature
+          textBody="Manage your fleet with ease, and automatically patch vulnerabilities when found."
+          textHeader="Orchestration & Automation"
+          image={orchImage}
+        ></Feature>
+      </Features>
+    </div>
+  );
+}
 
 export default Frontpage;
