95e77496dc65fc105896372388a67c51b27aadbf
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2022-02-26 22:04:35 -0500

    fix height issues

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -16,7 +16,7 @@ function Dashboard() {
     
     return (
     <NavigationFrame>
-      <iframe src='https://soteria-metrics.ml/d/bOYL0Bf7k/test-dashboard?orgId=2&from=1645499234224&to=1645500487912' width='100%' style={{height: "85vh"}}/>
+      <iframe src='https://soteria-metrics.ml/d/bOYL0Bf7k/test-dashboard?orgId=2&from=1645499234224&to=1645500487912' width='98%' height='95%' style={{margin: "1%"}}/>
     </NavigationFrame>
     );
   
