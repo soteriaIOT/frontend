e6e4a6627673e8eefa87682b2fcf31767c0ffc5e
Kai Huang <edwardthetrueblade2678@gmail.com>
2022-03-01 19:47:19 -0500

    read username/password from dynamo

--- src/components/login-sigup/index.tsx
+++ src/components/login-sigup/index.tsx
@@ -1,15 +1,30 @@
-import React, {useCallback, useState} from 'react';
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Modal, Stack, TextField, Form, ThemeProvider} from '@shopify/polaris';
+import { Modal, Stack, TextField, Form, ThemeProvider } from "@shopify/polaris";
+import * as AWS from "aws-sdk";
+import { AWSError } from "aws-sdk";
+import { GetItemOutput } from "aws-sdk/clients/dynamodb";
 
-export default function LoginSignup({active, setActive}: {active: boolean, setActive: (value: boolean) => void}) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export default function LoginSignup({
+  active,
+  setActive,
+}: {
+  active: boolean;
+  setActive: (value: boolean) => void;
+}) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loginNotSignup, setLoginNotSignup] = useState(false);
 
   const navigate = useNavigate();
 
+  AWS.config.update({ region: "us-east-2" });
+  AWS.config.update({
+    accessKeyId: "AKIASM7RO3GR6EFRV2X3",
+    secretAccessKey: "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",
+  });
+  var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
   const handleModalChange = useCallback(() => setActive(!active), [active]);
 
@@ -17,31 +32,53 @@ export default function LoginSignup({active, setActive}: {active: boolean, setAc
     handleModalChange();
   };
 
-  const handleLoginNotSignupChange = useCallback(() => setLoginNotSignup(!loginNotSignup), [loginNotSignup]);
+  const handleLoginNotSignupChange = useCallback(
+    () => setLoginNotSignup(!loginNotSignup),
+    [loginNotSignup]
+  );
 
   const handleSubmit = useCallback(() => {
-    console.log("submitted", name, email, password, loginNotSignup);
-    setEmail('');
-    setName('');
-    setPassword('');
-    handleModalChange();
-    navigate("/dashboard")
-  }, []);
-
+    var params = {
+      TableName: "login-information",
+      Key: {
+        username: { S: email },
+      },
+    };
+    console.log(password);
+    ddb.getItem(params, function (err: AWSError, data: GetItemOutput) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log(data);
+        console.log(data.Item);
+        if (data.Item) {
+          console.log(data.Item.password.S);
+        }
+        if (data.Item && data.Item.password.S === password) {
+          console.log("Successfully authenticated.");
+          navigate("/dashboard");
+          handleModalChange();
+        } else {
+          console.log("Password is incorrect.");
+        }
+      }
+    });
+  }, [email, name, password]);
+  console.log(email);
 
   return (
-    <div style={{height: '500px'}}>
+    <div style={{ height: "500px" }}>
       <Modal
         open={active}
         onClose={handleClose}
         title="Login"
         primaryAction={{
-          content: loginNotSignup ? 'Login' : 'Signup',
+          content: loginNotSignup ? "Login" : "Signup",
           onAction: handleSubmit,
         }}
         secondaryActions={[
           {
-            content: loginNotSignup ? 'Sign up' : 'Login',
+            content: loginNotSignup ? "Sign up" : "Login",
             onAction: handleLoginNotSignupChange,
           },
         ]}
@@ -49,31 +86,33 @@ export default function LoginSignup({active, setActive}: {active: boolean, setAc
         <Modal.Section>
           <Stack vertical>
             <Stack.Item>
-                <Form onSubmit={handleSubmit}>
-                    {!loginNotSignup && <TextField
-                        requiredIndicator={true}
-                        label="Name"
-                        value={name}
-                        onChange={(value) => setName(value)}
-                        autoComplete="on"
-                    />}
-                    <TextField
-                        requiredIndicator={true}
-                        label="Email"
-                        type="email"
-                        value={email}
-                        onChange={(value) => setEmail(value)}
-                        autoComplete="on"
-                    />
-                    <TextField
-                        requiredIndicator={true}
-                        label="Password"
-                        type="password"
-                        value={password}
-                        onChange={(value) => setPassword(value)}
-                        autoComplete="off"
-                    />
-                </Form>
+              <Form onSubmit={handleSubmit}>
+                {!loginNotSignup && (
+                  <TextField
+                    requiredIndicator={true}
+                    label="Name"
+                    value={name}
+                    onChange={(value) => setName(value)}
+                    autoComplete="on"
+                  />
+                )}
+                <TextField
+                  requiredIndicator={true}
+                  label="Email"
+                  type="email"
+                  value={email}
+                  onChange={(value) => setEmail(value)}
+                  autoComplete="on"
+                />
+                <TextField
+                  requiredIndicator={true}
+                  label="Password"
+                  type="password"
+                  value={password}
+                  onChange={(value) => setPassword(value)}
+                  autoComplete="off"
+                />
+              </Form>
             </Stack.Item>
           </Stack>
         </Modal.Section>
