d551958e88efa982458f3f2cfad3f2315664a97a
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2022-03-09 12:15:04 -0500

    Address comments

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { setContext } from '@apollo/client/link/context';
 import App from './App';
 import './index.scss';
 
-const API_URL = "http://localhost:8081/query"
+const API_URL = "https://soteriaiot-api.herokuapp.com/query"
 
 
 const httpLink = new HttpLink({
@@ -37,8 +37,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
